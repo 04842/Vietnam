c67464a176f3797f9203cb434c8591d52b32b55c
ac842 <130188187+ac842@users.noreply.github.com>
2024-09-21 07:27:01 +0800

    Initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,25 @@
 // src/pages/index.tsx
 
 import React from 'react'
-import { graphql, HeadFC } from 'gatsby'
+import { graphql, HeadFC, PageProps } from 'gatsby'
 import Layout from '../components/Layout'
 import Seo from '../components/Seo'
 import SearchBar from '../components/SearchBar'
 import TravelRecommendations from '../components/TravelRecommendations'
 import Promotions from '../components/Promotions'
 
-interface HomePageProps {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string
-        description: string
-      }
-    }
+interface SiteMetadata {
+  title: string
+  description: string
+}
+
+interface HomePageData {
+  site: {
+    siteMetadata: SiteMetadata
   }
 }
+
+type HomePageProps = PageProps<HomePageData>
 
 const HomePage: React.FC<HomePageProps> = ({ data }) => {
   const { title } = data.site.siteMetadata
@@ -35,7 +37,7 @@ const HomePage: React.FC<HomePageProps> = ({ data }) => {
 
 export default HomePage
 
-export const Head: HeadFC<HomePageProps> = ({ data }) => (
+export const Head: HeadFC<HomePageData> = ({ data }) => (
   <Seo title={data.site.siteMetadata.title} description={data.site.siteMetadata.description} />
 )
 
